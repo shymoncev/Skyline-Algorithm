25eec36eda49b2226c8eabe5ce59ec340edd73f7
shymoncev <167310901+shymoncev@users.noreply.github.com>
2024-07-29 22:51:41 +0900

    Update main.py
    중간 시각화 과정도 계속 만져보려 하곤 있는데 제 수준에선 쉽사리 잘 안되네요,, main쪽에서 그냥 스카이라인 선 잇는거랑 안꼬이게 sort로 x오름차순으로 해서 잡는걸로 바꿔 봤습니다ㅜㅜ  감사합니다

--- skyline_algorithm/divided_and_conquer/main.py
+++ skyline_algorithm/divided_and_conquer/main.py
@@ -6,6 +6,7 @@ from src.skyline.divide_and_conquer import m_partitioning_dnc
 def main():
     datas = Points(size=(2, 1000))  # point = 1000 개
     skyline = m_partitioning_dnc(datas.data)
+    skyline_sorted = sorted(skyline, key=lambda point: point[0])
     # skyline = dnc(datas.data)
     fig = go.Figure()
     fig.add_trace(
@@ -18,13 +19,14 @@ def main():
             )
         )
     fig.add_trace(
-        go.Scatter(x=[data[0] for data in skyline],
-                   y=[data[1] for data in skyline],
-                   mode='markers',
-                   name='skyline',
-                   marker=dict(size=15)
-                   )
-                   )
+    go.Scatter(
+        x=[data[0] for data in skyline_sorted],
+        y=[data[1] for data in skyline_sorted],
+        mode='lines+markers',
+        name='skyline',
+        marker=dict(size=15)
+    )
+)
     fig.show()
 
 
